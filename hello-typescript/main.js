b8d6bdae5d33cc35036ec69bae824249d76a320a
Danilo Aleixo <danilo_aleixo@hotmail.com>
2017-04-06 22:15:31 -0300

    typescript

---
+++ hello-typescript/main.js
@@ -0,0 +1,21 @@
+var minhaVar = 'minha variavel';
+function minhaFunc(x, y) {
+    return x + y;
+}
+var num = 1;
+var bla = 0;
+var numeros = [1, 2, 3];
+numeros.map(function (val) {
+    return val + 1;
+});
+numeros.map(function (val) { return val + 2; });
+var Matematica = (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
+var n1 = 'dsadasasdsa';
+n1 = 4;
