d9932ae8d1ca07bab646d9d1d87402a37c467ca5
Danilo Aleixo <danilo_aleixo@hotmail.com>
2017-04-12 23:51:51 -0300

    Aula 7

--- HelloWord/src/app/cursos/cursos.component.ts
+++ HelloWord/src/app/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service'
 
 @Component({
   selector: 'app-cursos',
@@ -8,14 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class CursosComponent implements OnInit {
 
 	nomePortal: string;
-	cursos: string[] = ['Java', 'Ext JS', 'Angular'];
+	cursos: string[];
 
 
-	constructor() {
-		this.nomePortal = "urll blaaaa";
+	constructor(private cursosService: CursosService) {
+		this.nomePortal = "urllll";
+
+		this.cursos = this.cursosService.getCursos();
+
 	}
 
 	ngOnInit() {
+
 	}
 
 }
