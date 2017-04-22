83ae8bee702b57d1b938141cef2901ec7de3b713
Danilo Aleixo <danilo_aleixo@hotmail.com>
2017-04-22 14:33:50 -0300

    Renomeei os projetos

---
+++ 03-data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+	url: string = 'http://google.com';
+	cursoAngular: boolean = true;
+	urlImagem: string = 'http://lorempixel.com/400/200/nature';
+
+	valorAtual: string = '';
+	valorSalvo: string = '';
+
+	isMouseOver: boolean = false;
+
+	nomeCurso: string = 'Angular2';
+
+	valorInicial: number = 15;
+
+
+	getValor(){
+		return 1;
+	}
+
+	getCurtirCurso(){
+		return true;
+	}
+
+
+	botaoClicado(){
+		alert("cliquei");
+	}
+
+	onKeyUp(event: KeyboardEvent){
+		this.valorAtual = (<HTMLInputElement>event.target).value;
+	}
+
+	salvarValor(valor){
+		this.valorSalvo = valor;
+	}
+
+	onMouseOverOut(){
+		this.isMouseOver = !this.isMouseOver;
+	}
+
+	onMudouValor(evento){
+		console.log(evento);
+	}
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
